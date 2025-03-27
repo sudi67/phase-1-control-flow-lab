998519f14dcae386d2f21529aa3abb97e163250c
sudeisahmed <ahmedsudeis324@gmail.com>
2025-03-27 10:15:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return `This one is on me!`;
+  }
+  if (ride > 400 && ride <= 2000){
+    return `That will be twenty bucks.`;
+  }
+  if (ride > 2000 && ride < 2500){
+    return `I will gladly take your thirty bucks.`;
+  }
+  if (ride > 2500){
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  switch(city){
+    case 'NYC':
+      return `Ok, sounds good.`;
+      break;
+    default:
+      return `No go.`
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return `Thank you so much.`;
+      break;
+    case 'not as generous':
+      return `Thank you.`;
+    default:
+      return `Bye.`;
+  }
 }
